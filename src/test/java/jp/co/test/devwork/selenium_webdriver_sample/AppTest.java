4b34ab0ee82ae5c4cdd235bbd2f1fc077df86f0f
systena <systena@192.168.142.22>
2020-05-29 09:53:08 +0900

    implememt test code

--- src/test/java/jp/co/test/devwork/selenium_webdriver_sample/AppTest.java
+++ src/test/java/jp/co/test/devwork/selenium_webdriver_sample/AppTest.java
@@ -1,38 +1,35 @@
 package jp.co.test.devwork.selenium_webdriver_sample;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+	WebDriver driver;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Before
+	public void setUp() throws Exception {
+		DesiredCapabilities chrome = DesiredCapabilities.chrome();
+		driver = new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"),chrome);
+		driver.get("https://www.google.co.jp");
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void testCase() throws Exception {
+		Thread.sleep(10000);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
 }
